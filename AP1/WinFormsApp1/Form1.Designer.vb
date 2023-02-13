7cfd5c01e4a8f8c4cdf5f8cce2cf3f025b612b7a
fabio-rcs <fabiorsousa81@ua.pt>
2023-02-13 14:44:56 +0000

    Finished calculadora for class 1

--- AP1/WinFormsApp1/Form1.Designer.vb
+++ AP1/WinFormsApp1/Form1.Designer.vb
@@ -22,10 +22,96 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "B"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(374, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Result"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 190)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 35)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(335, 102)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(169, 100)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(527, 252)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- AP1/WinFormsApp1/Form1.vb
+++ AP1/WinFormsApp1/Form1.vb
@@ -1,3 +1,64 @@
-﻿Public Class Form1
+﻿'Nesta classe é definido todo o código necessário para a execução da interface (Janela) Form1
+Public Class Form1
+    'A subrotina Form1_Load é executada automaticamente, uma vez, quando a janela Form1 aparece no écra
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Esta função executada no arranque da aplicação
 
+        'Proprieades da Form1
+        Text = "Calculadora" 'A palavra Calculadora aparece na parte de cima da janela Form1
+        BackgroundImage = Image.FromFile("B:\source\repos\Inf_Indust\AP1\docs\ua-top-1.jpg") 'Esta passa a ser a imagem de fundo da janela Form1
+        'BackgroundImageLayout = ImageLayout.Stretch
+        Height = 200 'A janela Form1 ocupa 200 por 500 pixeis no écran
+        Width = 500
+        Icon = New Icon("B:\source\repos\Inf_Indust\AP1\docs\ua-favicon.ico") ' Esta imagem "icon" aparece no canto superior da janela Form1
+
+        'Propriedade da TextBox1
+        'A caixa de texto TextBox1, o seu canto superior esquerdo, tem as coordenadas X=50,Y=40
+        TextBox1.Location = New Point(50, 40)
+        TextBox1.Width = 100
+        TextBox1.Height = 30
+
+        TextBox2.Location = New Point(50, 120)
+        TextBox2.Width = 100
+        TextBox2.Height = 30
+
+        TextBox3.Location = New Point(300, 75)
+        TextBox3.Height = 100
+        TextBox3.Width = 30
+
+        Button1.Text = "+"
+        Button1.BackColor = Color.Red 'O botao passa a ter a cor de fundo vermelha
+        Button1.ForeColor = Color.Yellow 'e o texto a amarelo
+        Button1.Location = New Point(175, 75)
+        Button1.Visible = True
+        Button1.Height = 40
+        Button1.Width = 100
+
+        Label1.Text = "X"
+        Label1.BackColor = Color.Yellow 'A cor de fundo do Label1 passa a amarelo
+        Label1.ForeColor = Color.Black 'e o texto que aparece no Label1, neste caso a letra X, passa preto
+        Label1.Width = 100
+        Label1.Location = New Point(50, 20)
+
+        Label2.Text = "Y"
+        Label2.BackColor = Color.Yellow
+        Label2.ForeColor = Color.Black
+        Label2.Width = 100
+        Label2.Location = New Point(50, 100)
+
+        Label3.Text = "Z"
+        Label3.BackColor = Color.Yellow
+        Label3.ForeColor = Color.Black
+        Label3.Width = 100
+        Label3.Location = New Point(300, 55)
+    End Sub
+
+    ' Quando o utilizador do programa permir o botao (+) da interface Form1 esta subrotina será executada
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Os conteúdos das caixas de texto da esquerda são convertidos em números inteiros
+        ' e o resutado da sua soma é visulizado na caixa de texto da direita (textbox3)
+        If IsNumeric(TextBox1.Text) And IsNumeric(TextBox2.Text) Then
+            TextBox3.Text = CInt(TextBox1.Text) + CInt(TextBox2.Text)
+        End If
+    End Sub
 End Class
