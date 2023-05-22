3bf0d75a14be4cce2b17485b3a746afe1726efb6
Fábio Sousa <fabiorsousa81@ua.pt>
2023-05-22 10:32:37 +0100

    Finished Class number 9, html page

--- Aulas_P/Aula_9/II_TP9_84400/WindowsApp1/Form1.Designer.vb
+++ Aulas_P/Aula_9/II_TP9_84400/WindowsApp1/Form1.Designer.vb
@@ -22,10 +22,67 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.TimerConnection = New System.Windows.Forms.Timer(Me.components)
+        Me.txtbxMessageReceived = New System.Windows.Forms.RichTextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TimerConnection
+        '
+        '
+        'txtbxMessageReceived
+        '
+        Me.txtbxMessageReceived.Location = New System.Drawing.Point(12, 12)
+        Me.txtbxMessageReceived.Name = "txtbxMessageReceived"
+        Me.txtbxMessageReceived.Size = New System.Drawing.Size(392, 397)
+        Me.txtbxMessageReceived.TabIndex = 0
+        Me.txtbxMessageReceived.Text = ""
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 424)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(416, 26)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(153, 20)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(153, 20)
+        Me.ToolStripStatusLabel2.Text = "ToolStripStatusLabel2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(416, 450)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtbxMessageReceived)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TimerConnection As Timer
+    Friend WithEvents txtbxMessageReceived As RichTextBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
 End Class
--- Aulas_P/Aula_9/II_TP9_84400/WindowsApp1/Form1.vb
+++ Aulas_P/Aula_9/II_TP9_84400/WindowsApp1/Form1.vb
@@ -1,3 +1,166 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
 
+Public Class Form1
+    ' The IP address of the server
+    ReadOnly ip As String = GetIp()
+    ReadOnly local_address As IPAddress = IPAddress.Parse(ip)
+
+    ' A connection object will receive data
+    Dim connection As New TcpClient
+
+    ' A server object will accept connection requests
+    ReadOnly server As New TcpListener(local_address, 80)
+
+    ReadOnly html_folder_path As String = "C:\Users\fabio\OneDrive\Documentos\GitHub\II---Informatica-Industrial\Aulas_P\Aula_9\II_TP9_84400\WindowsApp1\HTML\"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Reservoir Control - Server"
+
+        txtbxMessageReceived.ScrollBars = RichTextBoxScrollBars.ForcedVertical
+        txtbxMessageReceived.HideSelection = False
+
+        TimerConnection.Enabled = True
+
+        ' Start the TCP server.
+        ' Will wait for clients to connect
+        server.Start()
+    End Sub
+
+    Private Sub TimerConnection_Tick(sender As Object, e As EventArgs) Handles TimerConnection.Tick
+
+        ' Display connection status
+        ToolStripStatusLabel1.Text = If(connection.Connected, "Connection Established", "Not Connected")
+        ToolStripStatusLabel2.Text = "IP " & local_address.ToString & " | Port 80"
+
+        'See if there's any connection request and accept if exists
+        If server.Pending Then
+            connection = server.AcceptTcpClient()
+        End If
+
+        ' If connected, get message
+        If connection.Connected Then
+
+
+            ' Get number of available bytes
+            Dim message_size As Integer = connection.Available()
+
+            ' Message must have buffer size bigger than 0
+            If message_size > 0 Then
+
+                Dim message_in_stream As NetworkStream = connection.GetStream()
+                Dim buffer(message_size) As Byte
+
+                'Read the bytes
+                message_in_stream.Read(buffer, 0, message_size)
+
+                Dim message_in As String = ""
+                Dim i As Integer
+
+                Dim data As String
+
+                ' Append each buffer to message_in
+                For i = 0 To message_size - 1
+#Disable Warning S1643 ' Strings should not be concatenated using "+" or "&" in a loop
+                    message_in += Chr(buffer(i))
+#Enable Warning S1643 ' Strings should not be concatenated using "+" or "&" in a loop
+                Next
+
+                ' Display message received
+                data = $"{Now}{" "}{message_in}"
+                txtbxMessageReceived.AppendText(data)
+                txtbxMessageReceived.Text += vbCrLf
+
+                ' Show always last message
+                Dim last As Long
+                last = Len(txtbxMessageReceived.Text)
+                txtbxMessageReceived.SelectionStart = last
+                '------------------------------------------------------
+                ' --------- Message Received Processing ---------------
+                '------------------------------------------------------
+                Dim first_line = message_in.Substring(0, message_in.IndexOf(vbCrLf))
+                Dim first_line_items = first_line.Split(" ")
+                'Dim method = first_line_items(0)
+                Dim path = first_line_items(1)
+
+                If InStr(path, "Supervision.html") Then
+                    SendFile(html_folder_path & "Supervision.html")
+
+                ElseIf InStr(path, "Supervisao.html") Then
+                    SendFile(html_folder_path & "Supervision.html")
+
+                ElseIf InStr(path, "Control.html") Then
+                    SendFile(html_folder_path & "Control.html")
+
+                ElseIf InStr(path, "Controlo.html") Then
+                    SendFile(html_folder_path & "Control.html")
+
+                ElseIf InStr(path, "Reservoir.html") Then
+                    SendFile(html_folder_path & "Reservoir.html")
+
+                ElseIf InStr(path, "Reservatorio.html") Then
+                    SendFile(html_folder_path & "Reservoir.html")
+
+                ElseIf InStr(path, "Rodape.html") Then
+                    SendFile(html_folder_path & "Rodape.html")
+
+                ElseIf InStr(path, "Reservoir2.html") Then
+                    SendFile(html_folder_path & "Reservoir2.html")
+
+                ElseIf InStr(path, "Reservatorio2.html") Then
+                    SendFile(html_folder_path & "Reservoir2.html")
+
+                ElseIf InStr(path, "Logo.html") Then
+                    SendFile(html_folder_path & "Logo.html")
+                End If
+            End If
+
+            'HTTP protocol foresees the end of the connection   
+            connection.Close()
+            connection = New TcpClient
+        End If
+
+    End Sub
+
+    Public Function GetIp() As String
+        Try
+            Dim IPList As System.Net.IPHostEntry = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName)
+            Dim IP As IPAddress
+
+            For Each IP In IPList.AddressList
+                'Only return IPv4 routable IPs 
+                If (IP.AddressFamily = Sockets.AddressFamily.InterNetwork) Then
+                    Return IP.ToString
+                End If
+            Next
+            Return ""
+
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Private Sub SendFile(ByVal file_path As String)
+        If connection.Connected Then
+            Dim file_stream As New StreamReader(file_path)
+            Dim file_string As String = file_stream.ReadToEnd()
+
+            Dim status = "HTTP/1.1 200 OK"
+            Dim header = "Content-Type: text/html; charset=wtf-8"
+
+            Dim answer = status & vbNewLine & header & vbNewLine & vbNewLine & file_string
+
+            Dim send_bytes As Byte() = Encoding.UTF8.GetBytes(answer)
+
+            Try
+                Dim stream As NetworkStream = connection.GetStream()
+                stream.Write(send_bytes, 0, send_bytes.Length)
+
+            Catch ex As Exception
+                MsgBox(ex.ToString())
+            End Try
+        End If
+    End Sub
 End Class
